780176a740c3fc50629bf23a7feb102566d8931f
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-03-24 10:49:06 +0100

    Create the basic project structure.

---
+++ setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+import re
+
+import setuptools
+
+PACKAGES = setuptools.find_packages(where='src')
+META_PATH = os.path.join('src', 'bio2bel_hmdd', '__init__.py')
+INSTALL_REQUIRES = [
+    'pybel>=0.11.1',
+    'click',
+    'sqlalchemy',
+    'bio2bel',
+    'pandas',
+    'tqdm'
+]
+EXTRAS_REQUIRE = {
+    'web': ['flask', 'flask_admin'],
+}
+ENTRY_POINTS = {
+    'bio2bel': [
+        'hmdd = bio2bel_hmdd',
+    ],
+    'console_scripts': [
+        'bio2bel_hmdd = bio2bel_hmdd.cli:main'
+    ]
+}
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Build an absolute path from *parts* and return the contents of the resulting file. Assume UTF-8 encoding."""
+    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """Extract __*meta*__ from META_FILE"""
+    meta_match = re.search(
+        r'^__{meta}__ = ["\']([^"\']*)["\']'.format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string'.format(meta=meta))
+
+
+def get_long_description():
+    """Get the long_description from the README.rst file. Assume UTF-8 encoding."""
+    with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name=find_meta('title'),
+        version=find_meta('version'),
+        description=find_meta('description'),
+        long_description=get_long_description(),
+        url=find_meta('url'),
+        author=find_meta('author'),
+        author_email=find_meta('email'),
+        maintainer=find_meta('author'),
+        maintainer_email=find_meta('email'),
+        license=find_meta('license'),
+        packages=PACKAGES,
+        package_dir={'': 'src'},
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE,
+        entry_points=ENTRY_POINTS,
+    )
---
+++ src/bio2bel_mir2disease/__init__.py
@@ -0,0 +1,61 @@
+
+
+
+# -*- coding: utf-8 -*-
+
+"""Bio2BEL mir2disease is a package which allows the user to work with the miR2Disease Database.
+
+
+Installation
+------------
+Get the Latest
+~~~~~~~~~~~~~~~
+Download the most recent code from `GitHub <https://github.com/bio2bel/hmdd>`_ with:
+
+.. code-block:: sh
+
+   $ python3 -m pip install git+https://github.com/bio2bel/hmdd.git
+
+For Developers
+~~~~~~~~~~~~~~
+Clone the repository from `GitHub <https://github.com/bio2bel/hmdd>`_ and install in editable mode with:
+
+.. code-block:: sh
+
+   $ git clone https://github.com/bio2bel/hmdd.git
+   $ cd hmdd
+   $ python3 -m pip install -e .
+
+
+Setup
+-----
+1. Create a :class:`bio2bel_hmdd.Manager` object
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+>>> from bio2bel_mir2disease import Manager
+>>> manager = Manager()
+
+2. Populate the database
+~~~~~~~~~~~~~~~~~~~~~~~~
+This step will take sometime since the HMDD data needs to be downloaded, parsed, and fed into the database line
+by line.
+
+>>> manager.populate()
+
+3. Get all associations to iterate and do magic
+
+>>> associations = manager.get_associations()
+"""
+
+from .manager import Manager
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_mir2disease'
+__description__ = "A package for converting the Human microRNA Disease Database (HMDD) to BEL."
+__url__ = 'https://github.com/bio2bel/hmdd'
+
+__author__ = 'Mehdi Ali, Dejan Dukic, and Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'MIT License'
+__copyright__ = 'Copyright (c) 2018 Mehdi Ali, Dejan Dukic, and Charles Tapley Hoyt'
