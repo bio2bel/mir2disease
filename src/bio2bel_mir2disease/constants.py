f0e2b9f282c2212dd4d7aaf70f995ec52c6eecfa
Mehdi Ali <mehdi.ali@scai.fraunhofer.de>
2018-03-24 11:16:12 +0100

    Add the constants needed for the mir2disease project

---
+++ src/bio2bel_mir2disease/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from bio2bel.utils import get_connection, get_data_dir
+
+MODULE_NAME = "mir2disease"
+DATA_DIR = get_data_dir(MODULE_NAME)
+DEFAULT_CACHE_CONNECTION = get_connection(MODULE_NAME)
+DATA_URL = "http://watson.compbio.iupui.edu:8080/miR2Disease/download/AllEntries.txt"
+DATA_FILE_PATH = os.path.join(DATA_DIR, "mir2diseaseRawData.tsv")
+col_names = ["miRNA ID", "MeSHDisease term", "Method", "Year", "Description"]
